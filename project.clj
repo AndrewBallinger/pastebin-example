18acf2e217c9e2c03dc1425c39a003dca8d33f8c
Andrew Ballinger <andrewballinger@stratisopt.com>
2017-05-06 19:59:19 -0700

    Initial implementation off a locally running dynamodb, still playing

--- project.clj
+++ project.clj
@@ -1,9 +1,13 @@
 (defproject paste-bin "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A simple paste application. Mother approved."
+  :url "paste.stratisopt.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [ring/ring-core "1.5.0"]
+                 [ring/ring-jetty-adapter "1.5.0"]
+                 [ring/ring-defaults "0.3.0"]
+                 [com.taoensso/faraday "1.9.0"]]
   :main ^:skip-aot paste-bin.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/paste_bin/core.clj
+++ src/paste_bin/core.clj
@@ -1,7 +1,50 @@
 (ns paste-bin.core
+  (require [ring.adapter.jetty :refer (run-jetty)]
+           [ring.middleware.defaults :refer :all]
+           [ring.middleware.x-headers :refer :all]
+           [clojure.pprint :refer (pprint)]
+           [taoensso.faraday :as far])
   (:gen-class))
 
+(def conn
+  "The client location"
+  {:access-key "Soup"
+   :secret-key "Secret"
+   :endpoint "http://localhost:8000"})
+
+(defn paste [id string]
+  (pprint id)
+  (pprint string)
+  (let [new-id (if (empty? id) (.toString (java.util.UUID/randomUUID)) id)]
+    (far/put-item conn :pastes {:id new-id :string string})
+    new-id))
+
+(defn on-post [request]
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body (paste (subs (:uri request) 1)
+                (slurp (:body request)))})
+
+(defn on-get [request]
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body (or (:string (far/get-item conn :pastes
+                           {:id (subs (:uri request) 1)}))
+             "")})
+
+(defn handler [request]
+  (pprint request)
+  (if (= (:request-method request) :post)
+    (on-post request)
+    (on-get request)))
+
+(def site
+  (-> handler
+      (wrap-defaults api-defaults)
+      (wrap-content-type-options :nosniff)
+      (wrap-frame-options :deny)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "When executed, runs a jetty server"
   [& args]
-  (println "Hello, World!"))
+  (run-jetty site {:port 3000 :join? false}))
